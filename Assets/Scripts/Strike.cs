265361e4be9245f5b29b262a5ea8c53c2e460429
SR198889 <SR198889@falmouth.ac.uk>
2021-02-02 18:20:05 +0000

    added a layer called Enemy and added multidirectional attacks, in doing so i added 3 attackpoints to the palyer Prefabs/Player.prefab

---
+++ Assets/Scripts/Strike.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strike : MonoBehaviour
+{
+    public Transform attackPointR;
+    public Transform attackPointUp;
+    public Transform attackPointDown;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            AttackR();
+        }
+        if (Input.GetKey(KeyCode.W) & Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            AttackUp();
+        }
+        if (Input.GetKey(KeyCode.S) & Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            AttackDown();
+        }
+    }
+    void AttackR()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPointR.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit Right " + enemy.name);
+        }
+    }
+    void AttackUp()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPointUp.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit up " + enemy.name);
+        }
+    }
+    void AttackDown()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPointDown.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit Down " + enemy.name);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPointR.position, attackRange);
+        Gizmos.DrawWireSphere(attackPointUp.position, attackRange);
+        Gizmos.DrawWireSphere(attackPointDown.position, attackRange);
+    }
+}
