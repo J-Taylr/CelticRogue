e6a413384064b6d7ddd8317f9a1e56b3f8840664
Bo Hands <BH189488@falmouth.ac.uk>
2021-02-05 13:10:38 +0000

    platform work

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public BoxCollider2D plat;
+    public SpriteRenderer sprite;
+    public Color nActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
