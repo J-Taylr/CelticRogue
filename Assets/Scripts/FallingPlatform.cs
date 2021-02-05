1d5b0e3094b753829ff7fd84a4cccd8ac0fdebe9
Bo Hands <BH189488@falmouth.ac.uk>
2021-02-05 16:48:27 +0000

    level functional (need a death state)

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -7,15 +7,37 @@ public class FallingPlatform : MonoBehaviour
     public BoxCollider2D plat;
     public SpriteRenderer sprite;
     public Color nActive;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Color yActive;
+    public float tbf; //Time before falling
+    public float tbr; //time before reappearing
+
+    void Start() {
+        sprite.color = yActive;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter2D(Collider2D player) {
+        if (player.tag == "Player")
+        {
+            Invoke("PlatformDestroy", tbf);
+        }
     }
+
+    void OnTriggerExit2D(Collider2D play) {
+        if (play.tag == "Player")
+        {
+            CancelInvoke("PlatformRegen");
+            Invoke("PlatformRegen",tbr);
+        }
+    }
+
+    void PlatformDestroy() {
+        sprite.color = nActive;
+        plat.enabled = false;
+    }
+
+    void PlatformRegen() {
+        plat.enabled = true;
+        sprite.color = yActive;
+    }
+   
 }
---
+++ Assets/Scripts/SpikeTP.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTP : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D player) {
+        if (player.tag == "Player")
+        {
+            print("Player is dead");
+        }
+    }
+}
