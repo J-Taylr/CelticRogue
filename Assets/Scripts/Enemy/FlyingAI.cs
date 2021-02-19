e56dca9868d69978ae501c0e4a1dc4b47b1b85e7
SR198889 <SR198889@falmouth.ac.uk>
2021-02-19 19:36:56 +0000

    work began on a flying enemy type, bugs still to be fixed

---
+++ Assets/Scripts/Enemy/FlyingAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingAI : MonoBehaviour
+{
+    public float speed;
+    public float Range;
+    public GameObject player;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        Vector2 direction = gameObject.transform.position - player.transform.position;
+
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+
+    }
+
+}
