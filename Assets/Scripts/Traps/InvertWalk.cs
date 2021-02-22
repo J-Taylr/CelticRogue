1b21c62dee7d91e400821bc55a163791b226ba6c
Bo Hands <BH189488@falmouth.ac.uk>
2021-02-22 18:37:23 +0000

    3rd draft 2/3rds done plus new traps and locks

---
+++ Assets/Scripts/Traps/InvertWalk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertWalk : MonoBehaviour
+{
+    void OnTriggerEnter2D (Collider2D check) {
+        if (check.tag == "Player")
+        {
+            check.gameObject.transform.rotation = Quaternion.Euler(0f,180f,180f);
+            check.gameObject.GetComponent<Rigidbody2D>().gravityScale = -4f;
+            check.gameObject.GetComponent<PlayerController>().jumpForce = -16f;
+            check.gameObject.GetComponent<PlayerController>().doubleJumpForce = -12f;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D leaf) {
+        if (leaf.tag == "Player")
+        {
+            leaf.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            leaf.gameObject.GetComponent<Rigidbody2D>().gravityScale = 4f;
+            leaf.gameObject.GetComponent<PlayerController>().jumpForce = 16f;
+            leaf.gameObject.GetComponent<PlayerController>().doubleJumpForce = 12f;
+        }
+    }
+}
---
+++ Assets/Scripts/Traps/Traper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traper : MonoBehaviour
+{
+    public GameObject door;
+
+    void OnTriggerEnter2D(Collider2D player) {
+        if (player.tag == "Player")
+        {
+            door.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
