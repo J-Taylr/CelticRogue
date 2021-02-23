b4cf6564b9bb7ea50a3b6797cb6926aab3c5021d
Bo Hands <BH189488@falmouth.ac.uk>
2021-02-23 16:32:05 +0000

    3rd Level draft done

---
+++ Assets/Scripts/Traps/RockSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+    public float time;
+    public GameObject rock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRock", time,time);
+    }
+
+    void SpawnRock() {
+        Instantiate(rock, gameObject.transform.position, Quaternion.identity);
+    }
+}
