96c12bc43f279df7905d8d10f5a1ee38d8f2447d
SR198889 <SR198889@falmouth.ac.uk>
2021-02-24 18:37:25 +0000

    finished the functionality for the big enemy attack but the damage function still needs to be called.

--- Assets/Scripts/BigEnemyScript.cs
+++ Assets/Scripts/BigEnemyScript.cs
@@ -12,6 +12,7 @@ public class BigEnemyScript : MonoBehaviour
     public bool attacking = false;
     public Rigidbody2D RB;
 
+    public bool stomp = false;
     public bool moving = true;
     public bool A;
     public BoxCollider2D trigger;
@@ -33,55 +34,71 @@ public class BigEnemyScript : MonoBehaviour
     {
         if (A == true)
         {
-            State1();
+            Hover();
         }
-        EnemyMove();
-        
+        else
+        {
+            EnemyMove();
+        }
     }
     public void EnemyMove()
     {
         if (moving == true)
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
-        }
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, whatIsPlayer);
-        if (groundInfo.collider == false )
-        {
-            if (movingRight == true)
+
+            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, whatIsPlayer);
+            if (groundInfo.collider == false)
             {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = false;
+                if (movingRight == true)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
+                print(groundInfo.collider);
             }
-            else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
-            }
-            print(groundInfo.collider);
         }
     }
-    public void State1()
+    public void Hover()
     {
         transform.position = new Vector2(player.transform.position.x , offsetY);
         StartCoroutine("Stomp");
+        StartCoroutine("Cooldown");
 
     }
-    public void State2()
+    public void Crash()
     {
-
+        RB.AddForce(new Vector2(0, -0.5f), ForceMode2D.Impulse);
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == ("Player"))
+        if (other.tag == ("Player") && (stomp == false))
         {
+            Debug.Log("trigger");
             moving = false;
             A = true;
+            stomp = true;
         }
     }
     IEnumerator Stomp()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1f);
+        Crash();
+        yield return new WaitForSeconds(2f);
+
+        
+    }
+    IEnumerator Cooldown()
+    {
+       
+        yield return new WaitForSeconds(5f);
+        stomp = true;
         A = false;
     }
 }
