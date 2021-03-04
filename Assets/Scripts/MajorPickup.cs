c7facc96f1cc80135a7b2eac7a2a78c78325df9c
Bo Hands <BH189488@falmouth.ac.uk>
2021-03-04 12:44:25 +0000

    Pickups can now be obtained in gameplay

---
+++ Assets/Scripts/MajorPickup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MajorPickup : MonoBehaviour
+{
+    public int pickupPower;
+
+    void OnTriggerEnter2D(Collider2D hit) {
+        if (hit.tag == "Player")
+        {
+            switch (pickupPower)
+            {
+                case 1:
+                    hit.GetComponent<Upgardes>().ranged = true;
+                    break;
+                case 2:
+                    hit.GetComponent<Upgardes>().dOTRanged = true;
+                    break;
+                case 3:
+                    hit.GetComponent<Upgardes>().slam = true;
+                    break;
+                case 4:
+                    hit.GetComponent<Upgardes>().dashEndExplosion = true;
+                    break;
+                case 5:
+                    hit.GetComponent<Upgardes>().dashAttack = true;
+                    break;
+                case 6:
+                    hit.GetComponent<Upgardes>().wallJumpExplosion = true;
+                    break;
+                case 7:
+                    hit.GetComponent<Upgardes>().walljumpDamageBoost = true;
+                    break;
+                case 8:
+                    hit.GetComponent<Upgardes>().wallJumpInvincibility = true;
+                    break;
+                case 9:
+                    hit.GetComponent<Upgardes>().rage = true;
+                    break;
+                case 10:
+                    hit.GetComponent<Upgardes>().dOTAttacker = true;
+                    break;
+                case 11:
+                    hit.GetComponent<Upgardes>().tempInvincibility = true;
+                    break;
+                default:
+                    break;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MajorUpgrades.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MajorUpgrades : MonoBehaviour
+{
+    public List<int> upgradeOrder;
+    int x;
+    public int noOfUpgrades;
+    public GameObject[] upgradesPickups;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upgradesPickups = GameObject.FindGameObjectsWithTag("MajorUpgrade");
+        for (int i = 0; i < noOfUpgrades; i++)
+        {
+            NewNumber();
+
+        }
+        for (int j = 0; j < upgradesPickups.Length; j++)
+        {
+            upgradesPickups[j].GetComponent<MajorPickup>().pickupPower = upgradeOrder[j];
+        }
+    }
+
+    void NewNumber() {
+        x = Random.Range(1,noOfUpgrades+1);
+        ApproveNumber();
+    }
+    void ApproveNumber() {
+        if (upgradeOrder.Contains(x))
+        {
+            NewNumber();
+        }
+        else
+        {
+            upgradeOrder.Add(x);
+        }
+    }
+}
