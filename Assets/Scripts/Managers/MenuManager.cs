83d2247ad8ea4ee52829f99209b73c0942c398c4
J-Taylor <jmarcustaylor@gmail.com>
2021-03-22 12:34:35 +0000

    title screen, instructions, menu music, in game music, menu sound effects, :)

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+ 
+    public GameObject instructionMenu;
+
+    public AudioSource menuMusic;
+    public AudioSource startGame;
+    public AudioSource openInst;
+    public AudioSource closeInst;
+
+    public void StartGame()
+    {
+        startGame.Play();
+        menuMusic.Stop();
+        Invoke("LoadScene", 1);
+    }
+
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenInstructions()
+    {
+        openInst.Play();
+        instructionMenu.SetActive(true);
+    }
+
+    public void CloseInstructions()
+    {
+        closeInst.Play();
+        instructionMenu.SetActive(false);
+    }
+
+
+
+}
