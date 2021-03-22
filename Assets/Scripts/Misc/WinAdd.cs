e846ed28aed37bfc684f201e178787608648ec57
Bo Hands <BH189488@falmouth.ac.uk>
2021-03-22 10:59:37 +0000

    temporary win condition added and start guide to double jump

---
+++ Assets/Scripts/Misc/WinAdd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinAdd : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D check) {
+        if (check.tag == "Player")
+        {
+            check.GetComponent<WinCheck>().points++;
+            check.GetComponent<WinCheck>().Check();
+        }
+    }
+}
---
+++ Assets/Scripts/Misc/WinCheck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCheck : MonoBehaviour
+{
+    public int points;
+    public GameObject winUI;
+
+    public void Check() {
+        if (points >= 3)
+        {
+            winUI.SetActive(true);
+        }
+    }
+}
