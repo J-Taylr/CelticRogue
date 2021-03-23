a73b15d577c03329847c20fa459392a544185745
SR198889 <SR198889@falmouth.ac.uk>
2021-03-23 15:14:46 +0000

    added in the AI pathing to tile map scene and made its so skulls find the target player location on start,

--- Assets/Prefabs/Imported Packages/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ Assets/Prefabs/Imported Packages/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -30,9 +30,12 @@ namespace Pathfinding {
 		void OnDisable () {
 			if (ai != null) ai.onSearchPath -= Update;
 		}
-
-		/// <summary>Updates the AI's destination every frame</summary>
-		void Update () {
+        void Start()
+        {
+            target = GameObject.Find("Player").transform;   
+        }
+        /// <summary>Updates the AI's destination every frame</summary>
+        void Update () {
 			if (target != null && ai != null) ai.destination = target.position;
 		}
 	}
