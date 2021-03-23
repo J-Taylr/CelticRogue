63a448aba70fb65a6ee67ad85d43c653bc658d74
J-Taylor <jmarcustaylor@gmail.com>
2021-03-23 13:39:53 +0000

    fixed falling rocks

--- Assets/Scripts/Traps/FallingRock.cs
+++ Assets/Scripts/Traps/FallingRock.cs
@@ -5,27 +5,44 @@ using UnityEngine;
 public class FallingRock : MonoBehaviour
 {
     public int damage;
-    public int rockHealth;
+    
 
-    public int destroyTimer;
-
+    public int destroyTime = 3;
+    float timeRemaining;
     void OnCollisionEnter2D(Collision2D check) {
 
-        if (rockHealth < 0)
-        {
-            Destroy(gameObject);
-        }
+       
         if (check.gameObject.tag =="Player")
         {
             check.gameObject.GetComponent<PlayerManager>().TakeDamage(damage,gameObject);
 
         }
-        rockHealth--;
+        
+    }
+
+    private void Awake()
+    {
+        timeRemaining = destroyTime;
+    }
+
+    private void Update()
+    {
+        TimerCountdown();
     }
 
 
 
-
-
+    public void TimerCountdown()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            timeRemaining = 0;
+            Destroy(gameObject);
+        }
+    }
 
 }
