0b030a3ecec92918ba7aea1a375d0ea3c3bb6555
J-Taylor <jmarcustaylor@gmail.com>
2021-03-23 10:39:58 +0000

    temporary janus pickup graphic

--- Assets/Scripts/Misc/WinCheck.cs
+++ Assets/Scripts/Misc/WinCheck.cs
@@ -7,10 +7,40 @@ public class WinCheck : MonoBehaviour
     public int points;
     public GameObject winUI;
 
+    public GameObject JanusOne;
+    public GameObject JanusTwo;
+    public GameObject JanusThree;
+
     public void Check() {
         if (points >= 3)
         {
             winUI.SetActive(true);
         }
     }
+
+    private void Update()
+    {
+        CheckJANUS();
+    }
+
+
+    public void CheckJANUS()
+    {
+        switch (points)
+        {
+            case 1:
+                JanusOne.SetActive(true);
+                break;
+
+            case 2:
+                JanusTwo.SetActive(true);
+                break;
+
+            case 3:
+                JanusThree.SetActive(true);
+                break;
+
+        }
+    }
+
 }
