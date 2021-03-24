8d9bc1222f150558fa4266841f71c65d84d1f856
Bo Hands <BH189488@falmouth.ac.uk>
2021-03-24 17:12:13 +0000

    tile map and reload level on death

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -40,5 +41,6 @@ public class GameManager : MonoBehaviour
     public void RespawnPlayer()
     {
         player.transform.position = spawn.position;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
