c1976d80b18164e2510090b6b0e9233b1c189c1d
SR198889 <SR198889@falmouth.ac.uk>
2021-04-13 16:43:18 +0100

    created the stone guardian enemy type and created a wiggle animation to help players identify when it is attacking

--- Assets/StoneGuard.cs
+++ Assets/StoneGuard.cs
@@ -4,32 +4,99 @@ using UnityEngine;
 
 public class StoneGuard : MonoBehaviour
 {
-    public GameObject pos;
-    public float speed;
+    public BoxCollider2D Bc;
+
     public GameObject Player;
+    public GameObject pos;
+
+    public float speed;
+    public float power;
+
+    public int KB;
+    public int damage;
+
+    private Rigidbody2D R;
+    private Rigidbody2D PR;
+    public bool M;
+
+    public Animator A;
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         pos = GameObject.Find("CrushPos");
+        R = this.GetComponent<Rigidbody2D>();
+        PR = Player.GetComponent<Rigidbody2D>();
+        A = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+      if (M == true)
+        {
+            MoveTo();
+        }
     }
-    private void OnTriggerEnter2D(Collider2D other)
+
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("hit");
-            MoveTo();
+            M = true;
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            other.gameObject.GetComponent<PlayerManager>().currentHealth -= damage;
+            if (transform.position.x <= other.transform.position.x)
+            {
+                Debug.Log("Smack");
+                PR.AddForce(new Vector2(KB, 10), ForceMode2D.Impulse);
+            }
+            
+           if (transform.position.x > other.transform.position.x)
+            {
+                Debug.Log("Smack2");
+                PR.AddForce(new Vector2(-KB, 10), ForceMode2D.Impulse);
+            }
+            StartCoroutine("K");
         }
     }
     void MoveTo()
     {
         float step = speed * Time.deltaTime;
-        Vector2.MoveTowards(transform.position, pos.transform.position, step);
+        transform.position = Vector2.MoveTowards(transform.position, pos.transform.position, step);
+        StartCoroutine("Crush");
+
+    }
+    IEnumerator Crush()
+    {
+        A.SetBool("Shake", true);
+        yield return new WaitForSeconds(3);
+        A.SetBool("Shake", false);
+        M = false;
+        Attack();
+        StartCoroutine("CD");
+    }
+    IEnumerator K()
+    {
+        Player.layer = 11;
+        yield return new WaitForSeconds(3);
+        Player.layer = 8;
+
+    }
+    IEnumerator CD()
+    {
+        Bc.enabled = false;
+        yield return new WaitForSeconds(3);
+        Bc.enabled = true;
+    }
+    void Attack()
+    {
+        R.AddForce(-transform.up * power, ForceMode2D.Impulse);
     }
 }
