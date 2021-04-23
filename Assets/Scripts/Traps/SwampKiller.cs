4f9ff5d7fad32e7544b3f53c273322bb1560afe9
BO HANDS <BH189488@falmouth.ac.uk>
2021-04-23 15:52:29 +0100

    2nd section done and swamps now do damage

---
+++ Assets/Scripts/Traps/SwampKiller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwampKiller : MonoBehaviour
+{
+    public int damage;
+    public float timeBetweenDamage;
+    public PlayerManager player;
+
+    private void OnTriggerEnter2D(Collider2D check)
+    {
+        if (check.tag == "Player")
+        {
+            player = check.GetComponent<PlayerManager>();
+            InvokeRepeating("SwampDamage",0f,timeBetweenDamage);
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player = null;
+            CancelInvoke();
+        }
+    }
+
+    public void SwampDamage() {
+        player.TakeDamage();
+    }
+}
