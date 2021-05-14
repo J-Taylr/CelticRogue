2adde801407fdc4bda56c683ee8e0f0e6b7be158
J-Taylor <jmarcustaylor@gmail.com>
2021-05-14 17:33:28 +0100

    Upgrades fixed for new UI

--- Assets/HealthUpgrader.cs
+++ Assets/HealthUpgrader.cs
@@ -7,29 +7,60 @@ public class HealthUpgrader : MonoBehaviour
 {
     PlayerManager playerManager;
 
+    public Button button;
+    public Animator anim;
     public Image spriteHolder;
     public Image fadeHolder;
     public Sprite[] Sprites;
-   
 
-    public int spriteChoice = 0;
+
+    public int currentstage = 0;
 
     public bool isHealth;
 
 
     private void Start()
     {
+        button = GetComponentInChildren<Button>();
+        anim = GetComponent<Animator>();
         playerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
+    }
+
+    private void Update()
+    {
+        if (currentstage >= 10)
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
+
+    public void CheckPoints()
+    {
+        if (playerManager.UpgradePoints > 0 && currentstage < 10)
+        {
+            if (isHealth)
+            {
+                anim.SetTrigger("HealthUpgrade");
+            }
+            else
+            {
+                anim.SetTrigger("DamageUpgrade");
+            }
+
+        }
+
     }
 
     public void ChangeSprite()
     {
-        UpgradePlayer();
-        spriteHolder.sprite = Sprites[spriteChoice];
-        fadeHolder.sprite = Sprites[spriteChoice];
 
-        
-        spriteChoice++;
+        UpgradePlayer();
+        spriteHolder.sprite = Sprites[currentstage];
+        fadeHolder.sprite = Sprites[currentstage];
+
+        playerManager.UpgradePoints--;
+        currentstage++;
+
     }
 
 
